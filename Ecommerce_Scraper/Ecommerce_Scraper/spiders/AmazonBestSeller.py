439fae5256c1450641959858e801a5aa98020664
Yash138 <58321226+Yash138@users.noreply.github.com>
2024-12-20 06:16:52 +0530

    Adjusted AmazonBestSelller Spider as per Postgres

--- Ecommerce_Scraper/Ecommerce_Scraper/spiders/AmazonBestSeller.py
+++ Ecommerce_Scraper/Ecommerce_Scraper/spiders/AmazonBestSeller.py
@@ -1,32 +1,63 @@
 import scrapy
 from datetime import datetime as dt
-from Ecommerce_Scraper.items import AmazonBestSellerItem
+from Ecommerce_Scraper.items import AmazonBestSellerMongoItem, AmazonBestSellerItem
 from Ecommerce_Scraper.utility import getCategoryUrls
+
+
+# class AmazonBestSellerSpider(scrapy.Spider):
+#     name = "AmazonBestSeller"
+#     allowed_domains = ["www.amazon.in"]
+#     collection_name = 'stg_amz__best_sellers'
+        
+#     def start_requests(self):
+#         self.category_urls_dict = {i['Url']:i for i in getCategoryUrls()}
+#         self.start_urls = list(self.category_urls_dict.keys())
+#         for url in self.start_urls:
+#             yield scrapy.Request(url, callback=self.parse)
+
+#     def parse(self, response):
+#         category = self.category_urls_dict[response.url.split('/ref')[0]]['Category']           # split the URL from ref - will work in case of Page 2 url
+#         subCategory = self.category_urls_dict[response.url.split('/ref')[0]]['SubCategory']     # split the URL from ref - will work in case of Page 2 url
+#         for i,product in enumerate(response.xpath('//div[contains(@id, "gridItemRoot")]')):
+#             item = AmazonBestSellerMongoItem()
+#             item['asin'] = product.xpath(f'//*[@id="p13n-asin-index-{i}"]/div/@data-asin').get()
+#             item['rank'] = int(product.xpath(f'//*[@data-asin="{item['asin'] }"]/div[1]/div[1]/span/text()').get().replace('#',''))
+#             item['productName'] = product.xpath(f'//*[@id="{item['asin']}"]/div/div/a/span/div/text()').get()
+#             item['productUrl'] = response.urljoin(product.xpath(f'//*[@id="{item['asin']}"]/div/div/a/@href').get()).split('/ref')[0]
+#             item['category'] = category
+#             item['subCategory'] = subCategory
+#             item['loadTimestamp'] = dt.now()
+#             yield item
+        
+#         next_page = response.css("li.a-last > a::attr(href)").get()
+#         if next_page:
+#             next_page_url = response.urljoin(next_page)
+#             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
 
 class AmazonBestSellerSpider(scrapy.Spider):
     name = "AmazonBestSeller"
     allowed_domains = ["www.amazon.in"]
-    collection_name = 'stg_amz__best_sellers'
+    table_name = 'staging.stg_amz__best_sellers'
         
     def start_requests(self):
-        self.category_urls_dict = {i['Url']:i for i in getCategoryUrls()}
+        self.category_urls_dict = {i['url']:i for i in getCategoryUrls(db='postgres')}
         self.start_urls = list(self.category_urls_dict.keys())
         for url in self.start_urls:
             yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        category = self.category_urls_dict[response.url.split('/ref')[0]]['Category']           # split the URL from ref - will work in case of Page 2 url
-        subCategory = self.category_urls_dict[response.url.split('/ref')[0]]['SubCategory']     # split the URL from ref - will work in case of Page 2 url
+        category = self.category_urls_dict[response.url.split('/ref')[0]]['category']           # split the URL from ref - will work in case of Page 2 url
+        subCategory = self.category_urls_dict[response.url.split('/ref')[0]]['sub_category']     # split the URL from ref - will work in case of Page 2 url
         for i,product in enumerate(response.xpath('//div[contains(@id, "gridItemRoot")]')):
             item = AmazonBestSellerItem()
             item['asin'] = product.xpath(f'//*[@id="p13n-asin-index-{i}"]/div/@data-asin').get()
             item['rank'] = int(product.xpath(f'//*[@data-asin="{item['asin'] }"]/div[1]/div[1]/span/text()').get().replace('#',''))
-            item['productName'] = product.xpath(f'//*[@id="{item['asin']}"]/div/div/a/span/div/text()').get()
-            item['productUrl'] = response.urljoin(product.xpath(f'//*[@id="{item['asin']}"]/div/div/a/@href').get()).split('/ref')[0]
+            item['product_name'] = product.xpath(f'//*[@id="{item['asin']}"]/div/div/a/span/div/text()').get()
+            item['product_url'] = response.urljoin(product.xpath(f'//*[@id="{item['asin']}"]/div/div/a/@href').get()).split('/ref')[0]
             item['category'] = category
-            item['subCategory'] = subCategory
-            item['loadTimestamp'] = dt.now()
+            item['sub_category'] = subCategory
+            item['load_timestamp'] = dt.now()
             yield item
         
         next_page = response.css("li.a-last > a::attr(href)").get()
