3b0d0824fa7788fad12ea028db4a3fadd55c8897
Yash138 <58321226+Yash138@users.noreply.github.com>
2025-02-27 02:57:40 +0530

    Utility | Get Paths from Nested Json

--- Amazon_Scraper/helpers/utils.py
+++ Amazon_Scraper/helpers/utils.py
@@ -20,3 +20,36 @@ def find_key_path(json_obj, target_key, path=None):
                 return result  # Return immediately once found
 
     return None  # Return None if the key is not found
+
+
+def extract_paths(data):
+    import pandas
+    
+    def dfs(data, path=None):
+        if path is None:
+            path = []
+        
+        results = []
+        
+        if isinstance(data, dict) and 'category_id' in data:
+            path.append(data['category_id'])
+        
+        subcategories = [v for k, v in data.items() if isinstance(v, dict) and 'category_id' in v]
+        
+        if subcategories:
+            for subcategory in subcategories:
+                results.extend(dfs(subcategory, path[:]))
+        else:
+            results.append(path)
+        
+        return results
+    output = []
+    for key, value in data.items():
+        for sub_key, sub_value in value.items():
+            output.extend(dfs(sub_value, [key]))
+
+    for i in output:
+        i.remove('bestsellers')
+    df = pandas.DataFrame(output, columns=['category']+['lvl'+str(i) for i in range(1, 21)])
+    df['data_node'] = df.iloc[:, 1:22].ffill(axis=1).iloc[:, -1]
+    return df.to_dict(orient='records')
