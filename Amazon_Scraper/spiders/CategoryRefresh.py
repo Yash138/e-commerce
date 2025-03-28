59404bfa4f0798cdcd3e2430f3b3f8e7000ec992
Yash138 <58321226+Yash138@users.noreply.github.com>
2025-03-29 01:24:46 +0530

    Refactored CategoryRefresh Spider

--- Amazon_Scraper/spiders/CategoryRefresh.py
+++ Amazon_Scraper/spiders/CategoryRefresh.py
@@ -7,77 +7,56 @@ class CategoryrefreshSpider(scrapy.Spider):
     name = "CategoryRefresh"
     allowed_domains = ["www.amazon.in"]
     start_urls = ["https://www.amazon.in/gp/bestsellers"]
+    # start_urls = ["https://www.amazon.in/gp/bestsellers/kitchen/1380441031"]
+    custom_settings = {
+        "ITEM_PIPELINES":{}
+    }
     
     def __init__(self, *args, **kwargs):
         self.items = dict()
         self.visited_url = set()
         
     def parse(self, response):
-        # for bs in response.xpath('//div[contains(@id, "CardInstance")]/div/ul/li/div/a'):
-        #     url = response.urljoin(bs.xpath("@href").get())
-        #     bs_category = url.split("/")[-1]
-        #     yield Request(
-        #             url=url,
-        #             callback=self.parse_item,
-        #             meta={"bs_category": bs_category}
-        #         )
-        bs_category = response.url.split("/")[-1]
+        bs_category = "bestsellers"
+        category_name = "Any Department"
         yield Request(
                 url=response.url,
                 callback=self.parse_item,
                 meta={
                     "bs_category": bs_category,
-                    "referer_url": response.url
+                    "category_name": category_name
                 }
             )
         
     def parse_item(self, response):
-        # flg = 0
         bs_category = response.meta.get("bs_category")
-        referer_url = response.meta.get("referer_url")
-        main_category_id = response.url.split("/")[-2]
+        category_name = response.meta.get("category_name")
         parent_category_id = response.url.split("/")[-1]
-        self.items.setdefault(bs_category, {})
-        for category in response.xpath("//div[@role='treeitem']"):
-            category_url = response.urljoin(category.xpath("./a/@href").get())
+        self.items.setdefault(bs_category, {"category_id":parent_category_id, "category_name":category_name})
+        for category in response.xpath(f'''
+            //div[@role="treeitem"]/span[contains(text(),"{category_name}")]/../following-sibling::div[@role="group"]/div[@role="treeitem"]
+        '''):
+            category_url = response.urljoin(category.xpath("./a/@href").get()).split("/ref")[0]
             category_name = category.xpath("./a/text()").get()
             category_id = category_url.split("/")[-1]
-            # main_category_id = category_url.split("/")[-2]
             if category_url not in self.visited_url:
-                # flg = 1
                 self.visited_url.add(category_url)
-                if main_category_id == 'gp':
-                    self.log("main_category_id is gp")
-                    # main_category_id = main_category_id if main_category_id != bs_category else category_id
-                    main_category_id = category_id
-                    self.items[bs_category].setdefault(main_category_id, {})
-                    self.items[bs_category][main_category_id] = {
-                        'category_id': category_id,
-                        "category_name": category_name
-                    }
-                else:
-                    key = ''
-                    for i in find_key_path(self.items, parent_category_id):
-                        key += '[' + str(i) + ']' if isinstance(i, int) else '[\'' + i + '\']'
-                    eval(f'self.items{key}')[str(category_id)] = {
-                        'category_id': category_id,
-                        "category_name": category_name
-                    }
+                key = ''
+                for i in find_key_path(self.items, parent_category_id):
+                    key += '[' + str(i) + ']' if isinstance(i, int) else '[\'' + i + '\']'
+                eval(f'self.items{key}')[str(category_id)] = {
+                    'category_id': category_id,
+                    "category_name": category_name
+                }
                 yield Request(
                     url=category_url,
                     callback=self.parse_item,
                     meta={
                         "bs_category":bs_category,
-                        "referer_url": response.url
+                        "category_name": category_name
                     }
                 )    
-            elif not category_name:
-                # we will not get the category_name and category_url:
-                # either it is the category one-level-up or
-                # we are at lowest level of category
-                pass
-        # self.log(f"###################Visited################### : {response.url}")
     def closed(self, reason):
         """Save categories to JSON when spider finishes"""
-        with open("categories_mapping.json", "w", encoding="utf-8") as f:
+        with open("./.json/categories_mapping3.json", "w", encoding="utf-8") as f:
             json.dump(self.items, f, indent=4)
