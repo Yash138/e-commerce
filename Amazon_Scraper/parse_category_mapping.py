84acd4566f6445bf95d5bf8df47f2975f23d2128
Yash138 <58321226+Yash138@users.noreply.github.com>
2025-03-29 17:54:38 +0530

    Updated Path

--- Amazon_Scraper/parse_category_mapping.py
+++ Amazon_Scraper/parse_category_mapping.py
@@ -5,27 +5,25 @@ from helpers.db_postgres_handler import AsyncPostgresDBHandler
 from settings import POSTGRES_HOST, POSTGRES_DATABASE, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_PORT
 
 
-with open('categories_mapping.json') as f:
+with open('./.json/categories_mapping3.json') as f:
     data = json.load(f)
     
-data_dict = extract_paths(data)
-if data_dict[0].get("category") == 'bestsellers':
-    data_dict.pop(0)
+if data.get("category") == 'bestsellers':
+    data = data['bestsellers']
 
-category_list = extract_categories(data['bestsellers'])
-if category_list[0].get("category_id") == 'bestsellers':
-    category_list.pop(0)
+data_dict = extract_paths(data)
+category_list = extract_categories(data)
 
 async def main():    
     db = AsyncPostgresDBHandler(POSTGRES_HOST, POSTGRES_DATABASE, POSTGRES_USERNAME, POSTGRES_PASSWORD, POSTGRES_PORT)
     await db.connect()
-    await db.bulk_upsert("curated.amz__category_hierarchy", data_dict, ['category', 'data_node'], update_columns=None)
+    await db.bulk_upsert("transformed.amz__category_hierarchy", data_dict, ['category', 'data_node'], update_columns=None)
     keys = set()
     for d in category_list:
         if d['category_id'] in keys:
             continue
         keys.add(d['category_id'])
-        await db.insert('curated.amz__category_hierarchy_flattened', d)
+        await db.insert('transformed.amz__category_hierarchy_flattened', d)
     
     await db.close()
 
